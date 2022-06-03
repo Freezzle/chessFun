a8f2e949f254a9cc34cf9866e3daddaaca47773b
Freezzle <dylan.claude.work@gmail.com>
2022-06-03 13:11:49 +0200

    Improvment

--- src/main/java/ch/claudedy/chess/basis/MoveStatus.java
+++ src/main/java/ch/claudedy/chess/basis/MoveStatus.java
@@ -3,10 +3,6 @@ package ch.claudedy.chess.basis;
 public enum MoveStatus {
     OK, BAD_MOVE_COMMAND, NO_PIECE_SELECTED, PIECE_BLOCKED, ENNEMY_PIECE_SELECTED, CANT_EAT_ENEMY_KING, PIECE_ILLEGAL_MOVE, PIECE_CANT_EAT_ALLY_PIECE, CANT_MOVE_DURING_ANOTHER_MOVE;
 
-    public boolean isAnError() {
-        return !isOk();
-    }
-
     public boolean isOk() {
         return this == OK;
     }
