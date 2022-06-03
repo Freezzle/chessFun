5a4b468bcd6f6cc5828bebd7af70adde38bd0a94
Freezzle <dylan.claude.work@gmail.com>
2022-06-03 15:12:28 +0200

    Improvment

--- src/main/java/ch/claudedy/chess/ui/InfoPlayer.java
+++ src/main/java/ch/claudedy/chess/ui/InfoPlayer.java
@@ -9,16 +9,11 @@ public class InfoPlayer {
     private final String elo;
     private final Color color;
     private final boolean isComputer;
-    private String piecesCaptured = "";
 
     public InfoPlayer(String name, String elo, Color color, boolean isComputer) {
         this.name = name;
         this.elo = elo;
         this.color = color;
         this.isComputer = isComputer;
-    }
-
-    public void replacePiecesCaptured(String piecesCaptured) {
-        this.piecesCaptured = piecesCaptured;
     }
 }
