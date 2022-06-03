7d6494fd912b31983d1112414d247168f481aa01
Freezzle <dylan.claude.work@gmail.com>
2022-06-03 12:51:38 +0200

    Improvment

--- src/main/java/ch/claudedy/chess/systems/SystemConfig.java
+++ src/main/java/ch/claudedy/chess/systems/SystemConfig.java
@@ -2,9 +2,9 @@ package ch.claudedy.chess.systems;
 
 public class SystemConfig {
     public static final boolean PRINT_CONSOLE = true;
-    public static final GameType GAME_TYPE = GameType.PLAYER_V_PLAYER;
-    public static final String ELO_COMPUTER = "2800";
+    public static final GameType GAME_TYPE = GameType.PLAYER_V_COMPUTER;
+    public static final String ELO_COMPUTER = "1200";
     public static final Boolean IS_WHITE_VIEW = true;
-    public static final long MOVETIME_STOCKFISH = 500l;
+    public static final long MOVETIME_STOCKFISH = 800l;
     public static final String BOARD = "fen/board-default.csv";
 }
